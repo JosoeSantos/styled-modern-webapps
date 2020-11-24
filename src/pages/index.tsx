46c9a38dc581861aab087ff0bbc28da40586aba6
arthurmalheiros <arthurgoncalvesmalheiros@gmail.com>
2020-11-24 15:35:00 -0300

    monorepo example included

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { Header } from "@shared/components";
+
 export default function Home() {
   return (
-    <div>
+    <>
+      <Header />
       <h1>Hello World</h1>
-    </div>
-  )
+    </>
+  );
 }
