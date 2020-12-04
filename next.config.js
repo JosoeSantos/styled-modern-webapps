70b5478c926959cab3bd0c2f31f434e7fc4dd46a
Josoe Santos Queiroz <jssantosqueiroz@gmail.com>
2020-12-04 16:01:57 -0300

    Remove problematic dependence

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const ESLintPlugin = require('eslint-webpack-plugin');
 const withImages = require('next-images');
 
 module.exports = withImages({
     webpack: (config) => {
         config.plugins = [
             ...config.plugins,
-            new ESLintPlugin()
         ]
         return config;
     }
